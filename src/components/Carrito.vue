0923f2b8f8687a97eb442c5915ea480e95a3dfc9
Juan Carlos Condori <juanc.com@gmail.com>
2020-05-24 14:41:23 -0400

    Adición de una función computada para sumar todos los precios de los items

--- src/components/Carrito.vue
+++ src/components/Carrito.vue
@@ -6,6 +6,9 @@
                 <button class="btn badge badge-danger float-right" @click="removerItem(item)">Eliminar</button>
             </li>
         </ul>
+        <div class="card p-3 my-5">
+            <h4 class="text-center" >Total: ${{ total }}</h4>
+        </div>
     </div>
 </template>
 
@@ -13,6 +16,11 @@
     export default {
         name: 'Carrito',
         props: ['items'],
+        computed:{
+            total(){
+                return this.items.reduce((acumulador, item)=> acumulador + Number(item.precio), 0)
+            }
+        },
         methods:{
             removerItem(item){
                 this.$emit('remover-item', item);
