2c5b77d9aca5d557ffa7a110a86469410d010b7e
Juan Carlos Condori <juanc.com@gmail.com>
2020-05-24 12:22:52 -0400

    Enviamos el listado de productos padre a hijo para poder visualizar cada producto

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <div id="app">
     <div class="row">
       <div class="col-md-7">
-        <div class="col-md-6">
-          <producto></producto>
+        <div class="row">
+          <div class="col-md-6" v-for="pro in productos" :key="pro.id">
+            <producto :producto="pro"></producto>
+          </div>
         </div>
       </div>
     </div> 
@@ -12,10 +14,16 @@
 
 <script>
 import Producto from './components/Producto.vue'
+import productos from'./productos.json' 
 export default {
   name: 'App',
   components: {
     Producto
+  },
+  data() {
+    return{
+      productos 
+    }
   }
 }
 </script>
